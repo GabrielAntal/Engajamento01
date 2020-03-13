9d03943f10428a80cc377f94b61e465234074082
Antal <gabrielantalsilva@gmail.com>
2020-03-12 22:56:16 -0300

    Alteracoes feitas para melhorar a arquitetura

--- Engajamento01/engajamento1/src/main/java/com/mycompany/engajamento1/Controller.java
+++ Engajamento01/engajamento1/src/main/java/com/mycompany/engajamento1/Controller.java
@@ -14,13 +14,32 @@ import java.util.Scanner;
  */
 
 public class Controller {
-    public Controller() {
+    //Fácil legibilidade
+    // Uso de recursos da linguagem
+    private static ArrayList<String> storage;
+    public static int end;
+    public static Scanner input;
+
+
+
+    public static void main(String [] args) {
+        //Controller secomp = new Controller();
+        //secomp.menu();
+        // variaveis declaradas todas juntas
+        storage = new ArrayList();
+        end = 1;
+        input = new Scanner(System.in);
+        menu();
+    }
+
+/*
+    public Controller()
+    {
         end = 1;
     }
-    private final ArrayList<String> storage = new ArrayList();
-    public int end;
-    public  Scanner input = new Scanner(System.in); 
-    public void menu(){
+  */
+
+    public static void menu(){
         while(end != 0){
             System.out.println("Secomp 2020-2\nStudents List:\nName: ");
             input.nextLine();
@@ -31,11 +50,18 @@ public class Controller {
                 cls();
             }    
         }
-        for(int i = 0; i<storage.size(); i++) {   
-        System.out.print(i + "- " + storage.get(i)+ "\n");
-        }    
+
+        // Maior utilizacao de metodos
+        listar(storage);
     }
-    public void cls() {
+
+
+    public static void listar(ArrayList<String> storage){
+        for(int i = 0; i<storage.size(); i++) {
+            System.out.print(i + "- " + storage.get(i)+ "\n");
+        }
+    }
+    public static void cls() {
         for (int i=0;i<100;i++)
             System.out.println();
     }
--- Engajamento01/engajamento1/src/main/java/com/mycompany/engajamento1/Secomp.java
+++ Engajamento01/engajamento1/src/main/java/com/mycompany/engajamento1/Secomp.java
@@ -3,15 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.engajamento1;
+//package com.mycompany.engajamento1;
 /**
  *
  * @author Aluno
  */
+
+//Variaveis declaradas todas jusntas e inicializadas no momento de execução do main
+
+/*
 public class Secomp{
     public static void main(String [] args) {
         Controller secomp = new Controller();
         secomp.menu();
     }
 }
-
+*/
